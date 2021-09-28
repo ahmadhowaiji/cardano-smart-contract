af2af3fb693ee2ccaec535e79d643467bfd63f57
ahmadhowaiji <ahmadhowaijigmail.com>
2021-09-28 17:29:58 +0300

    oracle main

--- oracle/app/Main.hs
+++ oracle/app/Main.hs
@@ -1,4 +1,12 @@
-module Main where
+import Cardano.Api (Error (displayError), writeFileTextEnvelope)
+import Data.Hex (Hex (hex))
+import Data.String (IsString (..))
+import Ledger (TokenName, TxId (TxId), TxOutRef (TxOutRef))
+import Ledger.Bytes (getLedgerBytes)
+import Plutus.V1.Ledger.Value (TokenName (TokenName))
+import PlutusNft (apiNFTMintScript)
+import System.Environment (getArgs)
+import Prelude
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
